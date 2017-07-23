79f9c16317dc07a897fb7bb2fc3a596012ddf2cc
AM <amitmishra009v@gmail.com>
2017-07-23 20:35:28 +0530

    env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -49,6 +50,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
